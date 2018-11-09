3b074c4cea6859fa1bd592d5e6ebd359973df254
OsugiTakuya <t.4.103.tkynt@gmail.com>
2018-11-09 11:45:35 +0900

    first commit by Osugi

---
+++ P2.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!-- http://localhost/gopesh/P2.php -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ページ２</title>
+    <link rel="stylesheet" type="text/css" href="css/P2_style.css">
+  </head>
+  <div id="wrap">
+    <!-- ======= ヘッダー ======= -->
+    <header>
+      <h1>タイトル</h1>
+    </header>
+
+    <!-- ======= コンテンツ ======= -->
+    <div id="contents">
+
+      <!-- イベント情報の表示 -->
+      <div class="box_event">
+        <div class="box1">
+          <!--<div class="box11">タイトル</div>
+          <div class="box12">カバディ大会</div>-->
+        </div>
+        <div class="box2">
+          <!--<div class="box21">日時</div>
+          <div class="box22">2018年11月2日（金）10時12分</div>-->
+        </div>
+        <div class="box3">
+          <!--<div class="box31">種目</div>
+          <div class="box32">バスケ</div>-->
+        </div>
+        <div class="box4">
+          <!--<div class="box41">募集人数</div>
+          <div class="box42">5人</div>-->
+        </div>
+        <div class="box5">
+          <!--<div class="box51">現在の人数</div>
+          <div class="box52">4人</div>-->
+        </div>
+        <div class="box6">
+          <!--<div class="box61">期限</div>
+          <div class="box62">2018年11月1日（木）10時00分</div>-->
+        </div>
+        <div class="box7">
+          <!--<div class="box71">代表者</div>
+          <div class="box72">角野達也</div>-->
+        </div>
+        <div class="box8">
+          <!--<div class="box81">連絡先</div>
+          <div class="box82">kdntty@gmail.com</div>-->
+        </div>
+        <div class="box9">
+          <!--<div class="box91">コメント</div>
+          <div class="box92">あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</div>-->
+        </div>
+      </div>
+
+      <!-- 希望者用入力フォーム -->
+      <div class="box_participant">
+        <div class="index_name">名前</div>
+        <div class="content_name">
+          <input type="text" name="" value="">
+        </div>
+        <div class="index_mail">メールアドレス</div>
+        <div class="content_mail">
+          <input type="text" name="" value="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+        </div>
+      </div>
+
+    </div>
+
+    <!-- ======= フッター ======= -->
+    <footer>
+      <h2>フッター</h2>
+    </footer>
+  </div>
+  </body>
+</html>
