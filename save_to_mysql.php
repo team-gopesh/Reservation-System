bada51adbae573ade45e3168d6bf079b5c2af558
OsugiTakuya <t.4.103.tkynt@gmail.com>
2019-01-10 11:02:37 +0900

    PHPで書いてた条件を削除

--- save_to_mysql.php
+++ save_to_mysql.php
@@ -36,92 +36,64 @@
     -->
 
     <?php
-    // 各入力フォームに正しく入力されているかの判定
-    $is_title = ($_POST['title'] != '');
-    $is_date = ($_POST['date'] != '');
-    $is_kind = ($_POST['kind'] != '');
-    $is_invite_people = ($_POST['invite_people'] != '');
-    $is_least_people = ($_POST['least_people'] != '');
-    $is_now_people = ($_POST['now_people'] != '');
-    $is_deadline = ($_POST['deadline'] != '');
-    $is_representative = ($_POST['representative'] != '');
-    $is_email = ($_POST['email'] != '');
-    $is_comment = ($_POST['comment'] != '');
+    // sql文で使う特殊文字をエスケープする関数
+    function quote_smart($value){
+      if (!is_numeric($value)){  //数値以外をクオートする
+        $value = "'" . mysql_real_escape_string($value) . "'";
+      }
+      return $value;
+    }
 
-    // 必要事項全てに記入されていれかの判定
-    $is_filled = ($is_title and $is_date and $is_kind
-      and $is_invite_people and $is_least_people and $is_now_people
-      and $is_deadline and $is_representative and $is_email and $is_comment
+    // MySQLの準備
+    require "./lib/MySQL_event_lib.php";
+    $link = start_MySQL();
+
+    // 現在登録されているレコードのidの最大値を取得
+    $id_current = mysql_query('select id from event order by id desc limit 1');
+    if(!$id_current){
+      die('現在登録されているidの取得に失敗しました。'.mysql_error());
+    }
+    while($row = mysql_fetch_assoc($id_current)){
+      if ($row == ''){   // データベースが空の場合
+        $id_last = 0;
+      } else {
+        $id_last = $row['id'];
+      }
+    }
+
+    // データベースへ保存する内容を整理
+    $add_id = $id_last + 1;
+    $add_title = quote_smart($_POST['title']);
+    $add_date = quote_smart($_POST['date']);
+    $add_kind = quote_smart($_POST['kind']);
+    $add_invite_people = quote_smart($_POST['invite_people']);
+    $add_least_people = quote_smart($_POST['least_people']);
+    $add_now_people = quote_smart($_POST['now_people']);
+    $add_deadline = quote_smart($_POST['deadline']);
+    $add_representative = quote_smart($_POST['representative']);
+    $add_email = quote_smart($_POST['email']);
+    $add_comment = quote_smart($_POST['comment']);
+
+    //INSERTするためのSQL文を作成
+    $sql = sprintf("insert into event (
+        id, title, date, kind, invite_people, least_people,
+        now_people, deadline, representative, email, comment
+      ) values (%d, %s, %d, %s, %d, %d, %d, %d, %s, %s, %s)",
+      $add_id, $add_title, $add_date, $add_kind, $add_invite_people, $add_least_people,
+      $add_now_people, $add_deadline, $add_representative, $add_email, $add_comment
     );
-
-    // 入力フォームに必要事項が全て入力されていたときの処理
-    if ($is_filled){
-      // sql文で使う特殊文字をエスケープする関数
-      function quote_smart($value){
-        if (!is_numeric($value)){  //数値以外をクオートする
-          $value = "'" . mysql_real_escape_string($value) . "'";
-        }
-        return $value;
-      }
-
-      // MySQLの準備
-      require "./lib/MySQL_event_lib.php";
-      $link = start_MySQL();
-
-      // 現在登録されているレコードのidの最大値を取得
-      $id_current = mysql_query('select id from event order by id desc limit 1');
-      if(!$id_current){
-        die('現在登録されているidの取得に失敗しました。'.mysql_error());
-      }
-      while($row = mysql_fetch_assoc($id_current)){
-        if ($row == ''){   // データベースが空の場合
-          $id_last = 0;
-        } else {
-          $id_last = $row['id'];
-        }
-      }
-
-      // データベースへ保存する内容を整理
-      $add_id = $id_last + 1;
-      $add_title = quote_smart($_POST['title']);
-      $add_date = quote_smart($_POST['date']);
-      $add_kind = quote_smart($_POST['kind']);
-      $add_invite_people = quote_smart($_POST['invite_people']);
-      $add_least_people = quote_smart($_POST['least_people']);
-      $add_now_people = quote_smart($_POST['now_people']);
-      $add_deadline = quote_smart($_POST['deadline']);
-      $add_representative = quote_smart($_POST['representative']);
-      $add_email = quote_smart($_POST['email']);
-      $add_comment = quote_smart($_POST['comment']);
-
-      //INSERTするためのSQL文を作成
-      $sql = sprintf("insert into event (
-          id, title, date, kind, invite_people, least_people,
-          now_people, deadline, representative, email, comment
-        ) values (%d, %s, %d, %s, %d, %d, %d, %d, %s, %s, %s)",
-        $add_id, $add_title, $add_date, $add_kind, $add_invite_people, $add_least_people,
-        $add_now_people, $add_deadline, $add_representative, $add_email, $add_comment
-      );
-      // データベースへ保存
-      $result_save = mysql_query($sql);
-      if(!$result_save){
-        die('INSERTクエリーが失敗しました。'.mysql_error());
-      }
-
-      //MySQLからの切断
-      quit_MySQL($link);
-
-      // 保存確認ページにリダイレクト
-      header('Location: check_save.php');
-      exit();
+    // データベースへ保存
+    $result_save = mysql_query($sql);
+    if(!$result_save){
+      die('INSERTクエリーが失敗しました。'.mysql_error());
     }
 
-    // 入力フォームに必要事項が入力されていないときの処理
-    else{
-      // 前のページにリダイレクト
-      //header('Location: page2_tatehata_1120.php');
-      //exit();
-    }
+    //MySQLからの切断
+    quit_MySQL($link);
+
+    // 保存確認ページにリダイレクト
+    header('Location: check_save.php');
+    exit();
 
     ?>
   </body>
